03e19f7f4611c19e866851d57192a4adcf2dfc4c
Mike Alves <michelalvs@gmail.com>
2022-04-05 19:11:09 -0300

    Innitial Project Commit

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    Game
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    transform.Translate(0.2f, 0, 0);              
+    }
+}
